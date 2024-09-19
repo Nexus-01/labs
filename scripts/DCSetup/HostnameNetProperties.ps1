8ca0d64cd1c683f2a185a17e53ea8455d700d3b6
Nexus-01 <137302676+Nexus-01@users.noreply.github.com>
2024-09-19 12:17:20 -0400

    HostnameNetProperties.ps1 - Initial commit

---
+++ scripts/DCSetup/HostnameNetProperties.ps1
@@ -0,0 +1,29 @@
+###
+#
+## configures network settings, renames the host, restarts the device.
+#
+###
+
+## configure network settings
+# adapter properties
+$adapterProperties = @{
+	InterfaceAlias = "Ethernet"
+	IPAddress = "192.168.3.11"
+	AddressFamily = "IPv4"
+	PrefixLength = 24
+	DefaultGateway = "192.168.3.254"
+}
+New-NetIPAddress @adapterProperties
+# set DNS server
+Set-DnsClientServerAddress -InterfaceAlias Ethernet -ServerAddresses 192.168.3.254
+
+
+## rename the host if not done already
+if ($(Get-ComputerInfo | Select-Object -ExpandProperty CsDNSHostName) -ne "DC01") {
+	Rename-Computer "DC01"
+} else {
+	Write-Host "Host already named to 'DC01'. Continuing..." -ForegroundColor Green
+}
+
+## restart for hostname change to take effect
+Restart-Computer
